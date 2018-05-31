9d406462aebd703521fb13e23fc9200dc30d26f8
Serhiy Artymovych <artymovich96@gmail.com>
2018-06-01 00:24:47 +0300

    Add

---
+++ tour/assets/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("head").append('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">');
+    $("head").append('<link rel="stylesheet" href="assets/css/form.css">');    
+    $("head").append('<link rel="stylesheet" href="assets/css/animate.css">');
+    $("head").append('<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">');
+    $("head").append('<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">');
+})
