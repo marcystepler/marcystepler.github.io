2276a4e995ee866aca9f518b31ae0c8f1fcc5771
Serhiy Artymovych <artymovich96@gmail.com>
2018-06-04 21:11:27 +0300

    Add

--- tour/assets/js/script.js
+++ tour/assets/js/script.js
@@ -5,3 +5,43 @@ $(document).ready(function() {
     $("head").append('<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">');
     $("head").append('<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">');
 })
+
+let add_button = document.getElementById('add_extension_button');
+let pay_text = document.getElementById('after_pay_text');
+let remove_button = document.getElementById('remove_extension_button');
+let order_full = document.getElementById('preorder_full');
+let order_basic = document.getElementById('preorder_basic');
+
+add_button.addEventListener('click', function(){
+	console.log('click\n');
+	
+	this.classList.add('hide');
+	this.classList.remove('show');
+
+	pay_text.classList.add('show');
+	pay_text.classList.remove('hide');
+	
+	order_basic.classList.remove('show');
+	order_basic.classList.add('hide');
+
+	order_full.classList.add('show');
+	order_full.classList.remove('hide');
+
+});
+
+remove_button.addEventListener('click', function(){
+	
+	pay_text.classList.remove('show');
+	pay_text.classList.add('hide');
+	
+	add_button.classList.add('show');
+	add_button.classList.remove('hide');
+
+
+	order_basic.classList.add('show');
+	order_basic.classList.remove('hide');
+
+	order_full.classList.remove('show');
+	order_full.classList.add('hide');
+});
+
