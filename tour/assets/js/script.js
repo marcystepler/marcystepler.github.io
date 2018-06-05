ea4c6b19dad91f0969aa96133d89fe92c86444c3
Serhiy Artymovych <artymovich96@gmail.com>
2018-06-05 21:44:40 +0300

    Add

--- tour/assets/js/script.js
+++ tour/assets/js/script.js
@@ -6,42 +6,95 @@ $(document).ready(function() {
     $("head").append('<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">');
 })
 
-let add_button = document.getElementById('add_extension_button');
-let pay_text = document.getElementById('after_pay_text');
-let remove_button = document.getElementById('remove_extension_button');
-let order_full = document.getElementById('preorder_full');
-let order_basic = document.getElementById('preorder_basic');
 
-add_button.addEventListener('click', function(){
-	console.log('click\n');
-	
-	this.classList.add('hide');
-	this.classList.remove('show');
+// 
+// 
+// First option choosen
+// 
 
-	pay_text.classList.add('show');
-	pay_text.classList.remove('hide');
-	
-	order_basic.classList.remove('show');
-	order_basic.classList.add('hide');
+let first_step = document.getElementById('first_step');
+let tab2 = document.getElementById('tab2');
+let first_options = document.getElementsByName('option'); 
 
-	order_full.classList.add('show');
-	order_full.classList.remove('hide');
+var choosen_option = 0;
+var auto_price = 0;
 
+let option_price_array = [3694, 4944, 4247, 4797];
+let auto_price_array = [1232, 1644, 1415, 1000];
+
+
+let span_price = document.getElementById('auto_price');
+
+first_step.addEventListener('click', function(){
+	// go to the next step
+	tab2.checked = true;
+	// see what option client had choose
+	for (opt of first_options){
+		if (opt.checked === true)
+			choosen_option = opt.value;
+	}
+	//check what price we need to show userr 
+	for (var i = 0; i < 4; i++)
+	{
+		if (choosen_option == option_price_array[i])
+			auto_price = auto_price_array[i];
+	}
+	// show right price to user
+	span_price.innerHTML = auto_price;
 });
 
-remove_button.addEventListener('click', function(){
+// 
+// 
+// Second option choosen
+// 
+
+
+let second_step = document.getElementById('second_step');
+let second_option = document.getElementById('option_auto1');
+
+second_step.addEventListener('click', function(){
+
+	document.getElementById('tab2').disabled = false;	
+	document.getElementById('tab3').checked = true;
+
+	for (opt of first_options){
+		if (opt.checked === true)
+			choosen_option = opt.value;
+	}
+	//get the first option 
+
+	auto_price = span_price.innerHTML;
+	//get the auto price
+
+	if (second_option.checked === true)
+		total = parseFloat(choosen_option) + parseFloat(auto_price);
+	else
+		total =  parseFloat(choosen_option);
+	// depend on user answe add or don't add the price 
+	document.getElementById('total').innerHTML = total;
 	
-	pay_text.classList.remove('show');
-	pay_text.classList.add('hide');
-	
-	add_button.classList.add('show');
-	add_button.classList.remove('hide');
+	console.log(total);
 
-
-	order_basic.classList.add('show');
-	order_basic.classList.remove('hide');
-
-	order_full.classList.remove('show');
-	order_full.classList.add('hide');
 });
+				
 
+// 
+// 
+// Synchronizing chekboxes
+// 
+
+$("input:checkbox").on('click', function() {
+    // in the handler, 'this' refers to the box clicked on
+	var $box = $(this);
+	if ($box.is(":checked")) {
+	// the name of the box is retrieved using the .attr() method
+	// as it is assumed and expected to be immutable
+	var group = "input:checkbox[name='" + $box.attr("name") + "']";
+	// the checked state of the group/box on the other hand will change
+	// and the current value is retrieved using .prop() method
+	$(group).prop("checked", false);
+	$box.prop("checked", true);
+	} else {
+	    $box.prop("checked", false);
+	}
+});
