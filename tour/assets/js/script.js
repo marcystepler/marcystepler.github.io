db469b46d06c713fafc2fffa67d854138e2a603c
Serhiy Artymovych <artymovich96@gmail.com>
2018-06-12 23:12:08 +0300

    Add

--- tour/assets/js/script.js
+++ tour/assets/js/script.js
@@ -12,13 +12,14 @@ $(document).ready(function() {
     $("head").append('<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">');
 })
 
-
 /*
  *
  *
  Execute when the first option (Choosee duration) choosen
  */
 
+
+let array_of_prices = [4894.06, 3605.28, 7210.56, 5626.02, 4172.29, 8344.58];
 /* 
 	array_of_prices = [	12d: single room, 
 							12d: room for two pp, 
@@ -28,27 +29,25 @@ $(document).ready(function() {
 							14d: room for two total  ]
 */
 
-let array_of_prices = [4894.06, 3605.28, 7210.56, 5626.02, 4172.29, 8344.58];
 let whole_sum = 0;
 let visa_tax = 288.91;
 
 document.getElementById('first_step').addEventListener('click', ()=>{
 	
-	/*go to the next tab*/
-	document.getElementById('tab4').checked = true;
+	//go to the next tab
+	document.getElementById('tab2').checked = true;
 	
 	/*reinitializing variables (in case user already pressed first step button and
 	returned to the first step)*/
 	array_of_prices = [4894.06, 3605.28, 7210.56, 5626.02, 4172.29, 8344.58];
-	visa_tax = 288.91;
-
-	/*see what option client had choose*/ 
+	
+	//see what option client had choose 
 	if (document.getElementById('option14').checked === true)
 		array_of_prices = array_of_prices.slice(3);
 	else
 		array_of_prices=array_of_prices.slice(0, 3);		
 
-	/*display the right info on the next tab*/
+	//display the right info on the next tab
 	document.getElementById('friendly_price').innerHTML = '$' + array_of_prices[1];
 	document.getElementById('discount').innerHTML = '$' +  parseInt(array_of_prices[0] - array_of_prices[1]);
 	document.getElementById('single_price').innerHTML =  '$' +  array_of_prices[0];
@@ -63,17 +62,19 @@ document.getElementById('first_step').addEventListener('click', ()=>{
 
 document.getElementById('fourth_step').addEventListener('click', ()=>{
 	
-	/*go to the next tab and make current tab clickable*/
-	document.getElementById('tab2').checked = true;
-	document.getElementById('tab4').disabled = false;	
+	//go to the next tab and make current tab clickable
+	document.getElementById('tab3').checked = true;
+	document.getElementById('tab2').disabled = false;	
 
-	/*depends on user choose throw out prices we don't need*/
+	//depends on user choose throw out prices we don't need
 	if(document.getElementById("option_friend_yes").checked === true){
 		array_of_prices = array_of_prices.slice(2);
-		visa_tax = visa_tax * 2;
+		visa_tax = 577.82;
 	}
-	else
+	else{
 		array_of_prices = array_of_prices.slice(0,1);
+		visa_tax = 288.91;
+	}
 
 	document.getElementById('visa_price').innerHTML = '$' + visa_tax;
 });
@@ -86,31 +87,24 @@ document.getElementById('fourth_step').addEventListener('click', ()=>{
 
  document.getElementById('second_step').addEventListener('click', ()=>{
 
-	/*go to the next tab and make current tab clickable*/
+	//go to the next tab and make current tab clickable
 
-	document.getElementById('tab2').disabled = false;	
-	document.getElementById('tab3').checked = true;
+	document.getElementById('tab3').disabled = false;	
+	document.getElementById('tab4').checked = true;
 
 	if (document.getElementById("option_visa_yes").checked === true)
 		whole_sum = array_of_prices[0] + visa_tax;		
 	else
 		whole_sum = array_of_prices[0];		
 
-	document.getElementById('total').innerHTML =  '$' +  whole_sum;
 	document.getElementById('pay_today').innerHTML =  '$' +  (whole_sum/100 * 40).toFixed(2);
-	document.getElementById('pay_next').innerHTML =  '$' +  (whole_sum/100 * 30).toFixed(2);
 
-});
+ 	next_payments = document.getElementsByClassName('pay_next');
+	
+	for (let i = 0; i < next_payments.length; i++){
+		next_payments[i].innerHTML = '$' +  (whole_sum/100 * 30).toFixed(2);
+	}
 
-/* 
- *
- * 
- Execute when the pay button is preseed
- */		
-
-document.getElementById('confirm_info').addEventListener('change', ()=>{
-	console.log("Submit");
-	document.getElementById('user_info').submit();
 });
 
 /*
@@ -123,11 +117,12 @@ let checkboxes = document.querySelectorAll("input[type='checkbox']");
 
 for (var i = 0; i < checkboxes.length; i++){
 
-	checkboxes[i].addEventListener('click', ()=>{		
-		let box = event.target;
+	checkboxes[i].addEventListener('click', function(e){		
+		
+		let box = e.target;
 	
 		if(box.checked === true){
-			/*choose all checkbox with the same name as clicked checkbox*/			
+			//choose all checkbox with the same name as clicked checkbox			
 			let group = document.querySelectorAll('[name="'+ box.name +'"]');
 			
 			for (let i = 0; i < group.length; i++){
@@ -158,6 +153,14 @@ var handler = StripeCheckout.configure({
 });
 
 document.getElementById('third_step').addEventListener('click', function(e) {
+
+/*  
+	Form that should be submited
+
+	document.getElementById('user_info').submit();
+
+*/
+
 // Open Checkout with further options:
 	handler.open({
 	    name: 'China tour',
@@ -166,6 +169,7 @@ document.getElementById('third_step').addEventListener('click', function(e) {
 	    amount: whole_sum * 100
 	});
 	e.preventDefault();
+
 });
 
 // Close Checkout on page navigation:
